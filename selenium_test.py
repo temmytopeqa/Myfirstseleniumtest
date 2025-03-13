aa7e287aa072e30dde0d6d1bd0c9d51a105bb02c
Temitope okikiolu Bello <temmytopeqa@gmail.com>
2025-03-13 10:03:49 +0100

    seleniumtestscript

--- selenium_test.py
+++ selenium_test.py
@@ -4,62 +4,62 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-#visit the website url
+# visit the website url
 driver.get("https://automationplayground.com/crm/login.html")
 time.sleep(5)
 
-#Login with a valid email address
+# Login with a valid email address
 driver.find_element(By.ID, "email-id").send_keys("obellotope@gmail.com")
 time.sleep(5)
 
-#Enter password
+# Enter password.......
 driver.find_element(By.ID,"password").send_keys("Qwerasd12@")
 time.sleep(5)
 
-#Click on "remember me"
+# Click on "remember me"
 driver.find_element(By.ID, "remember").click()
 time.sleep(5)
 
-#Click the submit button
+# Click the submit button
 driver.find_element(By.ID, "submit-id").click()
 time.sleep(5)
 
-#Click on "new customers"
+# Click on "new customers"
 driver.find_element(By.ID, "new-customer").click()
 time.sleep(5)
 
-#input new customer email address
+# input new customer email address
 driver.find_element(By.ID, "EmailAddress").send_keys("sundaynight@gmail.com")
 time.sleep(5)
 
-#input first name of customer
+# input first name of customer
 driver.find_element(By.ID, "FirstName").send_keys("sunday")
 time.sleep(5)
 
-#input last name of the new customer
+# input last name of the new customer
 driver.find_element(By.ID, "LastName").send_keys("night")
 time.sleep(5)
 
-#input city
+# input city
 driver.find_element(By.ID, "City").send_keys("Ikeja")
 time.sleep(5)
 
-#input state
+# input state....
 driver.find_element(By.ID, "StateOrRegion").send_keys("Lagos")
 time.sleep(5)
 
-#Select gender type
+# Select gender type
 driver.find_element(By.XPATH,"//*[@id='loginform']/div/div/div/div/form/div[6]/input[2]").click()
 time.sleep(5)
 
-#Check the "Add to promotional list" button
+# Check the "Add to promotional list" button
 driver.find_element(By.XPATH, "//*[@id='loginform']/div/div/div/div/form/div[7]/input").click()
 time.sleep(5)
 
-#Click on Submit
+# Click on Submit
 driver.find_element(By.XPATH, "//*[@id='loginform']/div/div/div/div/form/button").click()
 time.sleep(5)
 
-#Click on sign out
+# Click on sign
 driver.find_element(By.XPATH, "/html/body/nav/ul/li/a").click()
 time.sleep(5)
